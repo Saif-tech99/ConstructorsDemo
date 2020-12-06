a67bb403e80ac0d8baacdd6e9843fac60581c15d
Saif Al-adilee <saifa9697@gmail.com>
2020-12-06 15:33:12 +0100

    Add project files.

---
+++ ConstructorsDemo/Factuur.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsDemo
+{
+    public class Factuur
+    {
+        public int Id; //default waarde = 0
+        public string Artikel; // objecten default waarde is NULL
+        public float Prijs;
+        public double BTW;
+        public Factuur()
+        {
+            BTW = 21.00;
+        }
+
+        public Factuur(int id)
+            :this()//constucter chaen
+        {
+            Id = id;
+        }
+        public Factuur(int id, string artikel)
+            :this(id)
+        {
+            this.Id = id;
+            this.Artikel = artikel;
+        }
+
+        public Factuur(int id, string artikel, float prijs)
+            :this(id,artikel)
+        {
+            this.Id = id;
+            this.Artikel = artikel;
+            this.Prijs = prijs;
+        }
+
+    }
+}
---
+++ ConstructorsDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Factuur factuur = new Factuur();
+            ////var factuur = new Factuur();
+            //Console.WriteLine(factuur.Id);
+            //Console.WriteLine(factuur.Artikel);
+
+            //Factuur factuur = new Factuur(2001501, "Tafel");
+            //Console.WriteLine(factuur.Id);
+            //Console.WriteLine(factuur.Artikel);
+
+            Factuur factuur = new Factuur(2001501);
+            Console.WriteLine(factuur.Id);
+            Console.WriteLine(factuur.BTW);
+
+            //Factuur factuur = new Factuur();
+            //factuur.Id = 3000300;
+            //factuur.Artikel = "Tafel";
+            //Console.WriteLine("{0} {1}", factuur.Id, factuur.Artikel);
+
+            //Factuur factuur = new Factuur()
+            //{
+            //    Id = 234,
+            //    Artikel = "tafel",
+            //    Prijs = 1234.123
+            //};
+            //Console.WriteLine(factuur.Id);
+            //Console.WriteLine(factuur.Artikel);
+            //Console.WriteLine(factuur.Prijs);
+        }
+    }
+}
